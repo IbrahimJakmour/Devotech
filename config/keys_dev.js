36038232799f12203fd266b1c8746bb691e35c06
Bob <bob.jakmour@gmail.com>
2018-06-19 00:03:58 +0300

    added files

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //to make the object available outside the file
+  mongoURI: "mongodb://bob:123456@ds227740.mlab.com:27740/dv", //connection with mongodb
+  secretOrKey: "secret" //creating a secret key
+};
+
---
+++ form/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://bob:123456@ds227740.mlab.com:27740/dv',
+  googleClientID: '182317930926-6hur2t9dpll5pjtpqto28sbntevpihpp.apps.googleusercontent.com',
+  googleClientSecret: 'omAZ5cop_F4sZthSssuOkeCx'
+}
